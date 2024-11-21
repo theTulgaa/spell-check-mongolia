ab48fb7a0fd79c527c129a6aacc0ff3c9820c629
monhtulga <munkhtulga.es@gmail.com>
2024-11-21 17:18:33 +0800

    hadgalalt

--- back-end/server.py
+++ back-end/server.py
@@ -4,7 +4,7 @@ from spylls.hunspell import Dictionary
 import os
 import json
 from flask import Response
-import notebook
+from notebook import prediction_response
 import numpy as np
 
 app = Flask(__name__)
@@ -23,7 +23,8 @@ def index():
     words = transfer.split()
     for word in words:
         if not dictionary.lookup(word):  
-          suggestions[word] = list(dictionary.suggest(word))
+        #   suggestions[word] = list(dictionary.suggest(word))
+            suggestions[word] = 1
     response_data = {'suggestions': suggestions}
     json_data = json.dumps(response_data, ensure_ascii=False)
     return Response(json_data, content_type="application/json; charset=utf-8")
@@ -38,6 +39,18 @@ def receive_message():
     transfer = response_message
 
     return jsonify({"response": response_message})
+
+@app.route('/pred', methods=['POST'])
+def predict():
+    data = request.get_json() 
+    text = data.get('inputText')
+    
+    if not text:
+        return jsonify({'error': 'No input text provided'}), 400
+    prediction = prediction_response(text)
+    prediction_list = prediction.tolist() if isinstance(prediction, np.ndarray) else prediction
+
+    return jsonify({'prediction': prediction_list})
 
 if __name__ == "__main__":
   app.run(debug=True, port=8080)
